6145becb4264db5388b2d5144f4c83d9f0e783ca
tszwong <wongt@bu.edu>
2023-07-11 22:14:39 -0400

    major design changes and reorganization, added new image section, updated and improved site responsiveness

--- script.js
+++ script.js
@@ -3,20 +3,3 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
-
-window.addEventListener('scroll', reveal);
-function reveal(){
-  var reveals = document.querySelectorAll('.reveal');
-  for(var i = 0; i < reveals.length; i++){
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
-
-    if (revealtop < windowheight - revealpoint) {
-      reveals[i].classList.add('active');
-    }
-    else {
-      reveals[i].classList.remove('active');
-    }
-  }
-}
